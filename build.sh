b8725fea5eddbdbb02a15f09b8368bb6da47a9da
Himanshu Raj <himanshurajkush@gmail.com>
2021-01-13 10:03:34 +0530

    build : Updated according to MICode Wiki

--- build.sh
+++ build.sh
@@ -1,24 +1,20 @@
 #!/usr/bin/env bash
 # SPDX-License-Identifier: GPL-3.0-or-later
-# Copyright (C) 2018 Raphiel Rollerscaperers (raphielscape)
-# Copyright (C) 2018 Rama Bondan Prakoso (rama982)
-# Android Kernel Build Script
+# Android Phenix Kernel Build Script
 
-sudo apt install bc bash git-core gnupg build-essential \
-    zip curl make automake autogen autoconf autotools-dev libtool shtool python \
-    m4 gcc libtool zlib1g-dev flex bison libssl-dev
+sudo apt-get install git ccache automake flex lzop bison \
+gperf build-essential zip curl zlib1g-dev zlib1g-dev:i386 \
+g++-multilib python-networkx libxml2-utils bzip2 libbz2-dev \
+libbz2-1.0 libghc-bzlib-dev squashfs-tools pngcrush \
+schedtool dpkg-dev liblz4-tool make optipng maven libssl-dev \
+pwgen libswitch-perl policycoreutils minicom libxml-sax-base-perl \
+libxml-simple-perl bc libc6-dev-i386 lib32ncurses5-dev \
+x11proto-core-dev libx11-dev lib32z-dev libgl1-mesa-dev xsltproc unzip
 
-git clone https://android.googlesource.com/platform/prebuilts/gcc/linux-x86/aarch64/aarch64-linux-android-4.9 -b android-10.0.0_r35 --depth=1 stock
-git clone https://android.googlesource.com/platform/prebuilts/gcc/linux-x86/arm/arm-linux-androideabi-4.9 -b android-10.0.0_r35 --depth=1 stock_32
-
-#git clone https://github.com/revanth-24/AnyKernel3
-
-if [ ! -d clang ]; then
-    wget https://android.googlesource.com/platform/prebuilts/clang/host/linux-x86/+archive/refs/heads/master/clang-r383902c.tar.gz
-    mkdir -p clang/clang-r383902c
-    tar xvzf clang-r383902c.tar.gz -C clang/clang-r383902c
-    rm clang-r383902c.tar.gz
-fi
+#Clang and Gcc
+git clone https://android.googlesource.com/platform/prebuilts/gcc/linux-x86/aarch64/aarch64-linux-android-4.9 -b android-10.0.0_r35 --depth=1 toolchain
+git clone https://android.googlesource.com/platform/prebuilts/gcc/linux-x86/arm/arm-linux-androideabi-4.9 -b android-10.0.0_r35 --depth=1 toolchain-32
+git clone --depth=1 https://github.com/kdrag0n/proton-clang clang
 
 if [ ! -d libufdt ]; then
     wget https://android.googlesource.com/platform/system/libufdt/+archive/refs/tags/android-10.0.0_r35/utils.tar.gz
@@ -27,37 +23,15 @@ if [ ! -d libufdt ]; then
     rm utils.tar.gz
 fi
 
-KERNEL_DIR=$PWD
-KERN_IMG=$KERNEL_DIR/out/arch/arm64/boot/Image.gz-dtb
-ZIP_DIR=$KERNEL_DIR/AnyKernel3
-CONFIG=phoenix_user_defconfig
-CROSS_COMPILE="aarch64-linux-android-"
-CROSS_COMPILE_ARM32="arm-linux-androideabi-"
-PATH=:"${KERNEL_DIR}/clang/clang-r383902c/bin:${PATH}:${KERNEL_DIR}/stock/bin:${PATH}:${KERNEL_DIR}/stock_32/bin:${PATH}"
+#PATH=:"${KERNEL_DIR}/clang/clang-r383902c/bin:${PATH}:${KERNEL_DIR}/stock/bin:${PATH}:${KERNEL_DIR}/stock_32/bin:${PATH}"
 
+mkdir out
 export ARCH=arm64
-export CROSS_COMPILE
-export CROSS_COMPILE_ARM32
+export SUBARCH=arm64
+export DTC_EXT=dtc
+export CROSS_COMPILE=${PWD}/toolchain/bin/aarch64-linux-android-
+export CROSS_COMPILE_ARM32=${PWD}/toolchain-32/bin/arm-linux-androideabi-
+export PATH=$PWD/clang/bin:$PATH
 
-make O=out $CONFIG
-make -j$(nproc --all) O=out \
-                      ARCH=arm64 \
-                      CC=clang \
-CLANG_TRIPLE=aarch64-linux-gnu- \
-CROSS_COMPILE=aarch64-linux-android-
-
-if ! [ -a $KERN_IMG ]; then
-    echo "Build error!"
-    exit 1
-fi
-
-#cd $ZIP_DIR
-#make clean &>/dev/null
-#cd ..
-
-#cd $ZIP_DIR
-#cp $KERN_IMG zImage
-#make normal &>/dev/null
-#echo "Flashable zip generated under $ZIP_DIR."
-#cd AnyKernel3
-#zip -r warp_kernelbygautam.zip 
+make O=out REAL_CC=${PWD}/clang/bin/clang CLANG_TRIPLE=aarch64-linux-gnu- vendor/phoenix_user_defconfig
+make -j$(nproc --all) O=out REAL_CC=${PWD}/clang/bin/clang CLANG_TRIPLE=aarch64-linux-gnu- 2>&1 | tee kernel.log
